f6d4fe1a761e14cd8ab0caac985ed078ab750ab9
hollowdll <pixerchat@gmail.com>
2024-10-26 12:23:01 +0300

    add client config

---
+++ src/main/java/com/github/hakjdb/hakjdbjava/ClientConfig.java
@@ -0,0 +1,51 @@
+package com.github.hakjdb.hakjdbjava;
+
+public final class ClientConfig {
+    private final String defaultDB;
+    private final int requestTimeoutSeconds;
+    private final String password;
+    // TODO: SSL configs
+
+    private ClientConfig(String defaultDB, int requestTimeoutSeconds, String password) {
+        this.defaultDB = defaultDB;
+        this.requestTimeoutSeconds = requestTimeoutSeconds;
+        this.password = password;
+    }
+
+    public String getDefaultDB() {
+        return defaultDB;
+    }
+
+    public int getRequestTimeoutSeconds() {
+        return requestTimeoutSeconds;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static class Builder {
+        private String defaultDB = "default";
+        private int requestTimeoutSeconds = 10;
+        private String password;
+
+        public ClientConfig build() {
+            return new ClientConfig(defaultDB, requestTimeoutSeconds, password);
+        }
+
+        public Builder defaultDB(String defaultDB) {
+            this.defaultDB = defaultDB;
+            return this;
+        }
+
+        public Builder requestTimeoutSeconds(int requestTimeoutSeconds) {
+            this.requestTimeoutSeconds = requestTimeoutSeconds;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+    }
+}
---
+++ src/main/java/com/github/hakjdb/hakjdbjava/HakjDB.java
@@ -0,0 +1,11 @@
+package com.github.hakjdb.hakjdbjava;
+
+public class HakjDB {
+    public HakjDB(String host, int port) {
+        //
+    }
+
+    public HakjDB(String host, int port, ClientConfig config) {
+        //
+    }
+}
