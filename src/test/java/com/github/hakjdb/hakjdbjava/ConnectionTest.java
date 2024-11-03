92f6e7e5fcdd356bcfc99a82af93b9e7a2477229
hollowdll <pixerchat@gmail.com>
2024-11-03 20:04:17 +0200

    add integration test with testcontainers

--- src/test/java/com/github/hakjdb/hakjdbjava/ConnectionTest.java
+++ src/test/java/com/github/hakjdb/hakjdbjava/ConnectionTest.java
@@ -3,11 +3,8 @@ package com.github.hakjdb.hakjdbjava;
 import com.github.hakjdb.hakjdbjava.grpc.GrpcClient;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
--- src/test/java/com/github/hakjdb/hakjdbjava/HakjDBTest.java
+++ src/test/java/com/github/hakjdb/hakjdbjava/HakjDBTest.java
@@ -1,5 +1,29 @@
 package com.github.hakjdb.hakjdbjava;
 
-public class HakjDBTest {
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+public class HakjDBTest {
+    private final int containerPort = 12345;
+
+    @Container
+    public GenericContainer<?> hakjdbContainer = new GenericContainer<>("hakj/hakjdb:v1.2.0-alpine")
+            .withExposedPorts(containerPort);
+
+    @Test
+    public void echoRequest() {
+        Integer mappedPort = hakjdbContainer.getMappedPort(containerPort);
+        String containerHost = hakjdbContainer.getHost();
+        System.out.printf("[Testcontainers] HakjDB container running at: %s:%d\n", containerHost, mappedPort);
+
+        HakjDB hakjdb = new HakjDB(containerHost, mappedPort);
+        String message = "Hello World!";
+        String result = hakjdb.echo(message);
+        assertEquals(message, result);
+    }
 }
