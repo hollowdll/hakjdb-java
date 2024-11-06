e1b06c3e85c15eaf589815f9134dedb407ea4d51
hollowdll <pixerchat@gmail.com>
2024-11-06 20:36:53 +0200

    add ConfigDefaults class

---
+++ src/main/java/com/github/hakjdb/hakjdbjava/ConfigDefaults.java
@@ -0,0 +1,12 @@
+package com.github.hakjdb.hakjdbjava;
+
+public final class ConfigDefaults {
+    public static final String DEFAULT_DATABASE = "default";
+    public static final String DEFAULT_HOST = "localhost";
+    public static final int DEFAULT_PORT = 12345;
+    public static final int DEFAULT_REQUEST_TIMEOUT_SECONDS = 10;
+    public static final int DEFAULT_DISCONNECT_WAIT_TIME_SECONDS = 5;
+    public static final boolean DEFAULT_USE_PASSWORD = false;
+    public static final boolean DEFAULT_USE_TLS = false;
+    public static final boolean DEFAULT_USE_CLIENT_CERT_AUTH = false;
+}
