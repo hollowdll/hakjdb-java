bcaaa26251a3216c3e44e673b44a0083b1cc2c87
hollowdll <pixerchat@gmail.com>
2024-11-06 20:55:18 +0200

    add GrpcMetadataKeys

---
+++ src/main/java/com/github/hakjdb/hakjdbjava/grpc/GrpcMetadataKeys.java
@@ -0,0 +1,7 @@
+package com.github.hakjdb.hakjdbjava.grpc;
+
+public final class GrpcMetadataKeys {
+    public static final String AUTH_TOKEN = "auth-token";
+    public static final String DATABASE = "database";
+    public static final String API_VERSION = "api-version";
+}
